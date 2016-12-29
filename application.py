6515c1f7612890a74b2c71998ba22871568fda78
Pedro Farias <PFarias@Pedros-MacBook-Pro.local>
2016-12-29 11:55:06 -0200

    Committing changes to db

--- application.py
+++ application.py
@@ -450,7 +450,7 @@ def manager():
                     error="The input for sessions must be an integer.")
     
     # prepare to render template
-    db.execute("SELECT * FROM " + tableName + " ORDER BY delName ASC;")
+    db.execute("SELECT * FROM " + tableName + " ORDER BY delname ASC;")
     delegations = db.fetchall()
     db.execute("SELECT * FROM users WHERE id = %s;",
         (session["user_id"],))
